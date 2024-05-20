43e60d46e623e4fa1ecb0041332ae348ec19ff34
Ernesto Garcia del moral G <ernesto.garcia@orange.com>
2024-05-20 16:55:55 +0200

    minor mods

--- internal/controller/certificatecheck_controller.go
+++ internal/controller/certificatecheck_controller.go
@@ -16,6 +16,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	certcheckerv1alpha1 "github.com/egarciam/cert-checker-operator/api/v1alpha1"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 // CertificateCheckReconciler reconciles a CertificateCheck object
@@ -43,21 +46,47 @@ func (r *CertificateCheckReconciler) Reconcile(ctx context.Context, req ctrl.Req
 		return ctrl.Result{}, err
 	}
 
-	// Check certificate expiration dates
-	for _, certInfo := range instance.Spec.Certificates {
-		cert, err := r.getCertificate(certInfo.SecretName, certInfo.SecretNamespace)
-		if err != nil {
-			r.Log.Error(err, "Failed to get certificate", "SecretName", certInfo.SecretName, "SecretNamespace", certInfo.SecretNamespace)
-			continue
-		}
+	// List all secrets in the cluster
+	secretList := &corev1.SecretList{}
+	if err := r.Client.List(ctx, secretList); err != nil {
+		return ctrl.Result{}, err
+	}
 
-		// Check expiration date
-		if time.Until(cert.NotAfter) < 30*24*time.Hour {
-			r.Log.Info("Certificate is expiring soon", "SecretName", certInfo.SecretName, "SecretNamespace", certInfo.SecretNamespace, "ExpiresAt", cert.NotAfter)
+	// Check certificates in each secret
+	for _, secret := range secretList.Items {
+		for _, certData := range secret.Data {
+			certs, err := r.parseCertificates(certData)
+			if err != nil {
+				r.Log.Error(err, "Failed to parse certificate", "SecretName", secret.Name, "SecretNamespace", secret.Namespace)
+				continue
+			}
+
+			for _, cert := range certs {
+				if time.Until(cert.NotAfter) < 30*24*time.Hour {
+					r.Log.Info("Certificate is expiring soon",
+						"SecretName", secret.Name,
+						"SecretNamespace", secret.Namespace,
+						"ExpiresAt", cert.NotAfter)
+				}
+			}
 		}
 	}
 
-	return ctrl.Result{RequeueAfter: 24 * time.Hour}, nil
+	// // Check certificate expiration dates
+	// for _, certInfo := range instance.Spec.Certificates {
+	// 	cert, err := r.getCertificate(certInfo.SecretName, certInfo.SecretNamespace)
+	// 	if err != nil {
+	// 		r.Log.Error(err, "Failed to get certificate", "SecretName", certInfo.SecretName, "SecretNamespace", certInfo.SecretNamespace)
+	// 		continue
+	// 	}
+
+	// 	// Check expiration date
+	// 	if time.Until(cert.NotAfter) < 30*24*time.Hour {
+	// 		r.Log.Info("Certificate is expiring soon", "SecretName", certInfo.SecretName, "SecretNamespace", certInfo.SecretNamespace, "ExpiresAt", cert.NotAfter)
+	// 	}
+	// }
+
+	return ctrl.Result{RequeueAfter: 5 * time.Second}, nil
 }
 
 func (r *CertificateCheckReconciler) getCertificate(secretName, secretNamespace string) (*x509.Certificate, error) {
@@ -81,8 +110,45 @@ func (r *CertificateCheckReconciler) getCertificate(secretName, secretNamespace 
 	return cert, nil
 }
 
+// parseCertificates extracts certificates from the secret data
+func (r *CertificateCheckReconciler) parseCertificates(certData []byte) ([]*x509.Certificate, error) {
+	var certs []*x509.Certificate
+	var block *pem.Block
+	var rest = certData
+
+	for {
+		block, rest = pem.Decode(rest)
+		if block == nil {
+			break
+		}
+		if block.Type == "CERTIFICATE" {
+			cert, err := x509.ParseCertificate(block.Bytes)
+			if err != nil {
+				return nil, err
+			}
+			certs = append(certs, cert)
+		}
+	}
+
+	if len(certs) == 0 {
+		return nil, fmt.Errorf("no certificates found")
+	}
+
+	return certs, nil
+}
+
 func (r *CertificateCheckReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&certcheckerv1alpha1.CertificateCheck{}).
+		Watches(
+			&source.Kind{Type: &corev1.Secret{}},
+			&handler.EnqueueRequestForOwner{
+				OwnerType:    &certcheckerv1alpha1.CertificateCheck{},
+				IsController: true,
+			},
+		).
+		WithOptions(controller.Options{
+			MaxConcurrentReconciles: 1,
+		}).
 		Complete(r)
 }
